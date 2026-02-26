a2e6d990b66f893722cbe2a4f6ad1b15e0d4a0d4
H. S. Nam Bình <nahmsix2005@gmail.com>
2026-02-26 23:38:08 +0700

    feat: create Tenant and User models

---
+++ app/Models/Tenant.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Multitenancy\Models\Tenant as BaseTenant;
+
+class Tenant extends BaseTenant
+{
+    protected $fillable = [
+        'name',
+        'domain',
+        'database',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
