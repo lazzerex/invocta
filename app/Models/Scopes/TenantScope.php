5248b57fd9a808c271f32f78d291b2b129d412b8
H. S. Nam Bình <nahmsix2005@gmail.com>
2026-02-28 17:47:12 +0700

    feat: implement TenantScope

---
+++ app/Models/Scopes/TenantScope.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Scopes;
+
+use App\Models\Tenant;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class TenantScope implements Scope
+{
+    public function apply(Builder $builder, Model $model): void
+    {
+        if ($tenant = Tenant::current()) {
+            $builder->where($model->getTable() . '.tenant_id', $tenant->id);
+        }
+    }
+}
