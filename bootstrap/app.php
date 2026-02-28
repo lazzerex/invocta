63ff088c573963675aaf492e02f49046b698fcb0
H. S. Nam Bình <nahmsix2005@gmail.com>
2026-02-28 17:48:20 +0700

    feat: add tenant.user middleware alias for multi-tenancy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,9 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        // Multi-tenancy middleware
         $middleware->alias([
             'tenant' => \Spatie\Multitenancy\Http\Middleware\NeedsTenant::class,
+            'tenant.user' => \App\Http\Middleware\SetTenantFromUser::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
